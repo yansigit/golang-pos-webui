5ef09b12008f8d21eca6dafc782936584fd06656
Gem <44089734+yansigit@users.noreply.github.com>
2020-11-06 17:40:57 +0900

    renamed templates folder

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"time"
+)
 
 func main() {
 	app := iris.New()
 
-	app.Get()
+	tmpl := iris.Django("./templates", ".html")
+	tmpl.Reload(true)                             // reload templates on each request (development mode)
+	tmpl.AddFunc("greet", func(s string) string { // {{greet(name)}}
+		return "Greetings " + s + "!"
+	})
+
+	// tmpl.RegisterFilter("myFilter", myFilter) // {{"simple input for filter"|myFilter}}
+	app.RegisterView(tmpl)
+
+	app.Get("/", index)
+
+	// http://localhost:8080
+	app.Listen(":8080")
 }
 
+var startTime = time.Now()
+
 func index(ctx iris.Context) {
-	ctx.Text()
+	ctx.View("hi.html", iris.Map{
+		"title":           "Hi Page",
+		"name":            "iris",
+		"serverStartTime": startTime,
+	})
 }
